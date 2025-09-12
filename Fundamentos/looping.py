6ea7b1c134fcf95509296133afc8feee300c42e3
Lucas <prmarinsoficial@gmail.com>
2025-09-11 22:10:51 -0300

    Modificação de documento

--- Fundamentos/looping.py
+++ Fundamentos/looping.py
@@ -1,6 +1,6 @@
 # tags: looping,
 
-lista_filmes = ["Onde os fracos não tem vez", "Procurando o Nemo", "Interstella", "The whale", ]
+lista_filmes = ["Onde os fracos não tem vez", "Procurando o Nemo", "Interstella", "The whale"]
 
 for i in lista_filmes:
     print(i)
